0d959115174ae3ff407f6ec0d36781bdfd7fbb7e
xiaojinwei <xiaojinwei9@mail.com>
2014-12-27 17:24:26 +0800

    初始recb

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -9,8 +9,24 @@ import models.*;
 
 public class Application extends Controller {
 
-    public static void index() {
-        render();
-    }
+	 public static void index() {
+	        render();
+	    }
+
+	    public static void topMenu(){
+	    	render();
+	    }
+	    public static void com(){
+	    	render();
+	    }
+	    public static void pro(){
+	    	render();
+	    }
+	    public static void know(){
+	    	render();
+	    }
+	    public static void about(){
+	    	render();
+	    }
 
 }
